dbea063c6df2edf5c0725a3beac70eaef47de9cf
aldosiregar <muhammadaldorafausta@gmail.com>
2024-09-29 20:46:57 +0700

    tambah navbar dan sidenav

---
+++ js/animation/sidenav.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("Main").style.marginRight = "250px";
+}
+
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("Main").style.marginRight = "0";
+}
--- js/template/navbar.js
+++ js/template/navbar.js
@@ -3,9 +3,36 @@ const navbarTemplate =
 <div class="navbar">
     <a href="/index.html">home</a>
     <div class="split">
-        <a href="#">tiu</a>
-        <a href="/data/html/twk.html">twk</a>
-        <a href="#">top</a>
+        <div class="d-none d-md-block">
+            <div class="dropdown">
+                <button class="dropBtn">Soal</button>
+                <div class="dropdown-content">
+                    <a href="#">twk</a>
+                    <a href="#">tiu</a>
+                    <a href="#">top</a>
+                </div>
+            </div>
+            <a href="/data/html/twk.html">twk</a>
+            <a href="#">tiu</a>
+            <a href="#">top</a>
+        </div>
+        <div class="d-block d-md-none">
+            <button class="burgerBtn" onclick="openNav()">Menu</button>
+            <div id="mySidenav" class="burger-content">
+                <a href="javascript:void(0)" class="closeBtn" onclick="closeNav()">&times;</a>
+                <div class="dropdown">
+                    <button class="dropBtn" onclick="burgerDropBtn()">Soal</button>
+                    <div class="dropdown-content">
+                        <a href="#">twk</a>
+                        <a href="#">tiu</a>
+                        <a href="#">top</a>
+                    </div>
+                </div>
+                <a href="/data/html/twk.html">twk</a>
+                <a href="#">tiu</a>
+                <a href="#">top</a>
+            </div>
+        </div>
     </div>
 </div>
 `;
